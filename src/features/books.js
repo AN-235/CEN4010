9f9fc6fadf87a71125cc3591ac0c9f8219b96ea3
Subrina Myers <subrinamyers2004@gmail.com>
2026-04-09 00:57:35 -0400

    editing: src/features/books.js

--- src/features/books.js
+++ src/features/books.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../utils/database');
 
 // ---------------------------------------------------------
 // FEATURE 1: Book Browsing and Sorting\
